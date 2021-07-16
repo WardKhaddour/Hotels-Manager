cdc6eade8ebf157bd4c80dda96d980192bf3759f
Ward <wardkhaddot@gmail.com>
2021-07-16 16:21:39 +0300

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HotelsManager());
+}
+
+class HotelsManager extends StatelessWidget {
+  const HotelsManager({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
